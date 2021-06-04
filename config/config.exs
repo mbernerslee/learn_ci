0f8bf90dad71d425f1eebed87933fa65fab89f2b
berners <matthewbernerslee@gmail.com>
2021-06-04 15:10:00 +0100

    added scripts to build an elixir release

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 config :learn_ci,
   ecto_repos: [LearnCi.Repo]
--- config/dev.exs
+++ config/dev.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 # Configure your database
 config :learn_ci, LearnCi.Repo,
